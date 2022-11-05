fe21488cc585f7aea41eb7e55c53b3aefd9fee9d
Spelljinxer <reiden556@gmail.com>
2022-11-05 21:39:10 +0800

    O(n log n) soln

--- two-sum.py
+++ two-sum.py
@@ -1,9 +1,20 @@
 
-def two_sum(list, target):
-    for i in range(len(list)):
-        for j in range(i+1, len(list)):
-            if list[i] + list[j] == target:
-                return [i, j]
+
+def two_sum(nums, target):
+    #O(n log n) time complexity
+    #O(1) space
+    visited = {}
+    for index, value in enumerate(nums):
+        remaining = target - nums[index]
+
+        if remaining in visited:
+            return[index, visited[remaining]]
+        
+        remaining[value] = index
+    
+    
+
+    
 
 list = [2,7,11,15]
 target = 9
