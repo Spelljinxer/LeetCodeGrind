7d10651bc6fc660e42d3aa5ec67766b727e9c1da
Spelljinxer <reiden556@gmail.com>
2022-11-05 21:41:34 +0800

    doc

--- Easy/two-sum.py
+++ Easy/two-sum.py
@@ -1,4 +1,11 @@
+#Problem: https://leetcode.com/problems/two-sum/
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
 
 def two_sum(nums, target):
     #O(n log n) time complexity
@@ -12,10 +19,6 @@ def two_sum(nums, target):
         
         remaining[value] = index
     
-    
-
-    
-
 list = [2,7,11,15]
 target = 9
 
