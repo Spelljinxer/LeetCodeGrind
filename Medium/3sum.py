ed59bf46d0c3f56f7bd05c1940d91e221cf14ebb
Spelljinxer <reiden556@gmail.com>
2022-11-14 00:26:14 +0800

    3sum

---
+++ Medium/3sum.py
@@ -0,0 +1,42 @@
+#Problem: https://leetcode.com/problems/3sum/description/
+'''
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+'''
+
+from collections import defaultdict
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        neg = defaultdict(int)
+        pos = defaultdict(int)
+        zeros = 0
+        
+        for x in nums:
+            if x < 0:
+                neg[x] += 1
+            elif x > 0:
+                pos[x] += 1
+            else:
+                zeros += 1
+        
+        r = []
+        
+        if zeros:
+            for n in neg:
+                if -n in pos:
+                    r.append((0, n, -n))
+        
+            if zeros > 2:
+                r.append((0,0,0))
+
+        for set_a, set_b in ((neg, pos), (pos, neg)):
+            set_a_items = list(set_a.items())
+            for i, (x, q) in enumerate(set_a_items):
+                for x2, q2 in set_a_items[i:]:
+                    if x != x2 or (x == x2 and q > 1):
+                        if -x-x2 in set_b:
+                            r.append((x, x2, -x-x2))
+
+        return r
