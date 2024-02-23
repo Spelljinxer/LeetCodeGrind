9ccd4a2bad29008f2bd302efcc6aa5c27317a696
Spelljinxer <reiden556@gmail.com>
2024-02-23 22:52:10 +0800

    two more

---
+++ Easy/RomanToInt.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        char arr[7]={'I','V','X','L','C','D','M'};
+        int arr2[7]={1,5,10,50,100,500,1000};
+        int num=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<7;j++){
+                if(s[i]==arr[j]){
+                    if((s[i]=='I') || (s[i]=='X') || (s[i]=='C')){
+                        if(s[i+1]==arr[j+1]){
+                            num=num-arr2[j];
+                        }
+
+                        else if(s[i+1]==arr[j+2]){
+                            num=num-arr2[j];
+                        }
+
+                        else num+=arr2[j];    
+                    }
+                    else num+=arr2[j];
+                
+                }
+            }
+        }
+        return num;
+    }
+};
