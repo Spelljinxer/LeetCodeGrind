19a05907f2087d9795b06da3390ed790b1113c68
Spelljinxer <reiden556@gmail.com>
2024-02-23 21:40:21 +0800

    searchinsert

---
+++ Easy/FirstOccurenceIndex/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+* https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+* Given two strings needle and haystack, 
+* return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+public class Solution {
+    public static int StrStr(string haystack, string needle) {
+      
+    }
+
+
+    public static void Main(String [] args)
+    {
+      string haystack = "sadbutsad";
+      string needle = "sad"
+    }
+}
