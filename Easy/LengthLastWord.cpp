44183ef307b909461f3ec04b3d3d9df845dce28c
Spelljinxer <reiden556@gmail.com>
2024-02-25 00:19:45 +0800

    length of last word

---
+++ Easy/LengthLastWord.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+      int len = 0; 
+      int tail = s.length() - 1;
+      while (tail >= 0 && s[tail] == ' ')
+      {
+        tail--;
+      }
+      while (tail >= 0 && s[tail] != ' ')
+      {
+        len++;
+        tail--;
+      }
+      return len;
+    }
+};
