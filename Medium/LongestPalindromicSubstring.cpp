dc80207b8eff782dd6665d10142ed48a8f89f2cd
Spelljinxer <reiden556@gmail.com>
2024-02-27 02:15:09 +0800

    manacher's algo

--- Medium/LongestPalindromicSubstring.cpp
+++ Medium/LongestPalindromicSubstring.cpp
@@ -3,41 +3,42 @@
 
 using namespace std;
 
-class Solution { 
-private: 
-    bool check(string &s, int i, int j){
-        while(i<j){
-            if(s[i] != s[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
+class Solution {
+
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        vector<string> substring;
-        for(int i=0; i<n; i++){
-            string temp = "";
-            for(int j=i; j<n; j++){
-                temp += s[j];
-                substring.push_back(temp);
+    string longestPalindrome(string s) { 
+        if (s.length() <= 1) {
+            return s;
+        }
+        
+        int maxLen = 1;
+        std::string maxStr = s.substr(0, 1);
+        s = "#" + std::regex_replace(s, std::regex(""), "#") + "#";
+        std::vector<int> dp(s.length(), 0);
+        int center = 0;
+        int right = 0;
+        
+        for (int i = 0; i < s.length(); ++i) {
+            if (i < right) {
+                dp[i] = std::min(right - i, dp[2 * center - i]);
+            }
+            
+            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s[i - dp[i] - 1] == s[i + dp[i] + 1]) {
+                dp[i]++;
+            }
+            
+            if (i + dp[i] > right) {
+                center = i;
+                right = i + dp[i];
+            }
+            
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxStr = s.substr(i - dp[i], 2 * dp[i] + 1);
+                maxStr.erase(std::remove(maxStr.begin(), maxStr.end(), '#'), maxStr.end());
             }
         }
-        int max_len = 0;
-        string finalans = substring[0];
-        int m = substring.size();
-        for(int i=0; i<m; i++){
-            int s = substring[i].size();
-            if(check(substring[i], 0, s-1)){
-                if(s > max_len){
-                    max_len = s;
-                    finalans = substring[i];
-                }
-            }       
-        }
-        return finalans;
+        
+        return maxStr;
     }
 };
